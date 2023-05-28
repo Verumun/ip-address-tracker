f908234cfcbee27ae39f72d4e016e1fe15ab9d2f
Verumun <nevezekiel3@gmail.com>
2023-05-28 09:26:16 +0100

    imported the leaflet map

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,34 @@
 'use srict'
 
-const map = document.getElementById("")
+// const map = document.getElementById("")
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        const { latitude } = position.coords;
+        const { longitude } = position.coords;
+        console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+  
+        const coords = [latitude, longitude];
+  
+        map = L.map('map').setView(coords, 13);
+  
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        }).addTo(map);
+  
+        // This is for whenever we click on the map
+        map.on('click', function (mapE) {
+          mapEvent = mapE;
+  
+          // Remove hidden class on form
+          form.classList.remove('hidden');
+          inputDistance.focus();
+        });
+      },
+      function () {
+        alert('could not get location');
+      }
+    );  
+    }        
