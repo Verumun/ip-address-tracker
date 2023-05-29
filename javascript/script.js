e74eb252f8514236caa3e1e80269a1aa74ceeb40
Verumun <nevezekiel3@gmail.com>
2023-05-29 11:54:21 +0100

    modified the tracker using chatGPT

--- javascript/script.js
+++ javascript/script.js
@@ -29,69 +29,149 @@ ipInputEl.addEventListener('submit', function(){
 //     iconAnchor: [15, 15]
 // });
 
+// if (navigator.geolocation) {
+//   navigator.geolocation.getCurrentPosition(
+//     function (position) {
+//       const { latitude } = position.coords;
+//       const { longitude } = position.coords;
+//     //   console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+//       const coords = [latitude, longitude];
+
+//       let map = L.map("map").setView(coords, 13);
+
+//       L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+//         attribution:
+//           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//       }).addTo(map);
+
+//     },
+//     function () {
+//       alert("could not get location");
+//     }
+//     );
+    
+//     renderResults()
+// }
+
+
+
+// formEl.onsubmit = (e) => {
+//   e.preventDefault();
+
+//   fetch(`https://ipapi.co/${ipInputEl.value}/json/`)
+//     .then((res) => res.json())
+//     .then((data) => renderResults(data))
+//     .catch((error) => displayError(error));
+// };
+
+// fetch("https://ipapi.co/json/")
+//   .then((res) => res.json())
+//   .then((data) => renderResults(data))
+//   .catch((error) => displayError(error));
+
+
+// function renderResults(data) {
+//   if (data.error) {
+//     throw `${data.reason}`;
+//   }
+
+//   ipEl.textContent = data.ip;
+//   locationEl.textContent = `${data.city}, ${data.region}, ${data.country_name}`;
+
+//   if (data.utc_offset !== null) {
+//     timezoneEl.textContent =
+//       "UTC: " + data.utc_offset.slice(0, 3) + ":" + data.utc_offset.slice(3);
+//   } else {
+//     timezoneEl.textContent = data.timezone;
+//   }
+
+//   ispEl.textContent = data.org;
+//   map.setView([data.latitude, data.longitude], 13);
+//   marker.setLatLng([data.latitude, data.longitude]);
+//   marker.bindPopup(`<b>${data.ip}</b>`).openPopup();
+// }
+
+
+
+
+// function displayError(e) {
+//     // errorMsgEl.textContent = e;
+//     console.log(e)
+//     // modal.showModal();
+// }
+
+let map = null;
+let marker = null;
+
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-    //   console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-
+      const { latitude, longitude } = position.coords;
       const coords = [latitude, longitude];
-
-      let map = L.map("map").setView(coords, 13);
-
-      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
-
+      initializeMap(coords);
+      fetchGeoLocationData(coords);
     },
     function () {
-      alert("could not get location");
+      alert("Could not get location");
     }
   );
 }
 
+function initializeMap(coords) {
+  map = L.map("map").setView(coords, 13);
 
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  marker = L.marker(coords).addTo(map);
+}
+
+function fetchGeoLocationData(coords) {
+  fetch("https://ipapi.co/json/")
+    .then((res) => res.json())
+    .then((data) => renderResults(data, coords))
+    .catch((error) => displayError(error));
+}
 
 formEl.onsubmit = (e) => {
   e.preventDefault();
+  const ipAddress = ipInputEl.value;
 
-  fetch(`https://ipapi.co/${ipInputEl.value}/json/`)
+  fetch(`https://ipapi.co/${ipAddress}/json/`)
     .then((res) => res.json())
-    .then((data) => renderResults(data))
+    .then((data) => {
+      const coords = [data.latitude, data.longitude];
+      map.setView(coords, 13);
+      marker.setLatLng(coords);
+      renderResults(data, coords);
+    })
     .catch((error) => displayError(error));
 };
 
-fetch("https://ipapi.co/json/")
-  .then((res) => res.json())
-  .then((data) => renderResults(data))
-  .catch((error) => displayError(error));
-
-
-function renderResults(data) {
-  if (data.error) {
-    throw `${data.reason}`;
+function renderResults(data, coords) {
+  if (!data || data.error) {
+    throw new Error("Invalid data received");
   }
+
   ipEl.textContent = data.ip;
   locationEl.textContent = `${data.city}, ${data.region}, ${data.country_name}`;
+
   if (data.utc_offset !== null) {
     timezoneEl.textContent =
       "UTC: " + data.utc_offset.slice(0, 3) + ":" + data.utc_offset.slice(3);
   } else {
     timezoneEl.textContent = data.timezone;
   }
+
   ispEl.textContent = data.org;
-  map.setView([data.latitude, data.longitude], 13);
-  marker.setLatLng([data.latitude, data.longitude]);
+  map.setView(coords, 13);
+  marker.setLatLng(coords);
   marker.bindPopup(`<b>${data.ip}</b>`).openPopup();
 }
 
-
-
-
-function displayError(e) {
-    // errorMsgEl.textContent = e;
-    console.log(e)
-    // modal.showModal();
+function displayError(error) {
+  console.log(error);
 }
